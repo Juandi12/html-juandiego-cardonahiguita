4c0b0056787472f29068c071d73d7485e17019c5
Juandi12 <123569475+Juandi12@users.noreply.github.com>
2023-02-06 12:02:42 -0500

    variables

---
+++ app.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded",() => {
+  const cardsAdj = [
+    {
+        name: 'CABALLO',
+        img: 'imagenes/CABALLO.jpg'
+    }
+    {
+      name: 'agula4',
+      img: 'imagenes/agula4.jpg'
+  }
+  {
+    name: 'dinosaurio',
+    img: 'imagenes/dinosaurio.jpg'
+}
+{
+  name: 'jirafa8',
+  img: 'imagenes/jirafa8.jpg'
+}
+{
+  name: 'lobo7',
+  img: 'imagenes/lobo7.jpg'
+}
+{
+  name: 'panda',
+  img: 'imagenes/panda.jpg'
+}
+{
+  name: 'CABALLO',
+  img: 'imagenes/CABALLO.jpg'
+}
+{
+name: 'agula4',
+img: 'imagenes/agula4.jpg'
+}
+{
+name: 'dinosaurio',
+img: 'imagenes/dinosaurio.jpg'
+}
+{
+name: 'jirafa8',
+img: 'imagenes/jirafa8.jpg'
+}
+{
+name: 'lobo7',
+img: 'imagenes/lobo7.jpg'
+}
+{
+name: 'panda',
+img: 'imagenes/panda.jpg'
+}
+  ]
+});
