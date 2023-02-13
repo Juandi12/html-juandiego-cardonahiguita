7037303ce7aea22575a70060b2a8dbf094b68563
Juandi12 <123569475+Juandi12@users.noreply.github.com>
2023-02-13 11:47:38 -0500

    nuevo13

--- app.js
+++ app.js
@@ -1,52 +1,70 @@
-document.addEventListener("DOMContentLoaded",() => {
-  const cardsAdj = [
+document.addEventListener("DOMContentLoaded", () => {
+  const cardAdj = [
     {
-        name: 'CABALLO',
-        img: 'imagenes/CABALLO.jpg'
+      name: "CABALLO",
+      img: "imagenes/CABALLO.jpg"
+    },
+    {
+      name: "agula4",
+      img: "imagenes/agula4.jpg"
+    },
+    {
+      name: "dinosaurio",
+      img: "imagenes/dinosaurio.jpg"
+    },
+    {
+      name: "jirafa8",
+      img: "imagenes/jirafa8.jpg"
+    },
+    {
+      name: "lobo7",
+      img: "imagenes/lobo7.jpg"
+    },
+    {
+      name: "panda",
+      img: "imagenes/panda.jpg"
+    },
+    {
+      name: "CABALLO",
+      img: "imagenes/CABALLO.jpg"
+    },
+    {
+      name: "agula4",
+      img: "imagenes/agula4.jpg"
+    },
+    {
+      name: "dinosaurio",
+      img: "imagenes/dinosaurio.jpg"
+    },
+    {
+      name: "jirafa8",
+      img: "imagenes/jirafa8.jpg"
+    },
+    {
+      name: "lobo7",
+      img: "imagenes/lobo7.jpg"
+    },
+    {
+      name: "panda",
+      img: "imagenes/panda.jpg"
     }
-    {
-      name: 'agula4',
-      img: 'imagenes/agula4.jpg'
+  ];
+
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasEscogidas = [];
+  var cartasEscogidasId = [];
+  var cartasGanadas = [];
+
+  //----------------- lecture_03 ----------------------------------//
+  function crearTablero() {
+    for (let i = 0; i < cardAdj.length; i++) {
+      var carta = document.createElement("img");
+      carta.setAttribute("src", "images/reverso.png");
+
+      carta.setAttribute("data-id", i);
+      carta.addEventListener("click", voltearCarta);
+      cuadricula.appendChild(carta);
+    }
   }
-  {
-    name: 'dinosaurio',
-    img: 'imagenes/dinosaurio.jpg'
-}
-{
-  name: 'jirafa8',
-  img: 'imagenes/jirafa8.jpg'
-}
-{
-  name: 'lobo7',
-  img: 'imagenes/lobo7.jpg'
-}
-{
-  name: 'panda',
-  img: 'imagenes/panda.jpg'
-}
-{
-  name: 'CABALLO',
-  img: 'imagenes/CABALLO.jpg'
-}
-{
-name: 'agula4',
-img: 'imagenes/agula4.jpg'
-}
-{
-name: 'dinosaurio',
-img: 'imagenes/dinosaurio.jpg'
-}
-{
-name: 'jirafa8',
-img: 'imagenes/jirafa8.jpg'
-}
-{
-name: 'lobo7',
-img: 'imagenes/lobo7.jpg'
-}
-{
-name: 'panda',
-img: 'imagenes/panda.jpg'
-}
-  ]
 });
