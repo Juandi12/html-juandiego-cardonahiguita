34f0b4d51560f0c6d300b5cbe5bed468418870e2
Juandi12 <123569475+Juandi12@users.noreply.github.com>
2023-03-08 09:18:45 -0500

    OE

--- app.js
+++ app.js
@@ -60,11 +60,21 @@ document.addEventListener("DOMContentLoaded", () => {
   function crearTablero() {
     for (let i = 0; i < cardAdj.length; i++) {
       var carta = document.createElement("img");
-      carta.setAttribute("src", "images/reverso.png");
+      carta.setAttribute("src", "imagenes/reverso.png");
 
       carta.setAttribute("data-id", i);
       carta.addEventListener("click", voltearCarta);
       cuadricula.appendChild(carta);
     }
   }
+  function voltearCarta() {
+    var cardId = this.getAttribute("data-id");
+    cartasEscogidas.push(cardAdj[cardId].name);
+    cartasEscogidasId.push(cardId);
+    this.setAttribute("src", cardAdj[cardId].img);
+    if (cartasEscogidas.length === 2) {
+      setTimeout(verificarPareja, 1000);
+    }
+  }
+  crearTablero();
 });
